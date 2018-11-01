62bfa85c267a8ede546ece36c1672cdd489d38df
Krystian Żądło <krystianzadlo@fejm.pl>
2018-11-01 13:59:09 +0100

    Index controller and basic homepage view were added

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function indexAction()
+    {
+        return $this->render('homepage.html.twig');
+    }
+
+
+}
