eb524a35b270134ac9b71415f9d1be32b61aa155
Krystian Żądło <krystianzadlo@fejm.pl>
2018-11-04 21:11:23 +0100

    EntityNormalizer was implemented for supporting entity relationships

---
+++ src/Normalizer/EntityNormalizer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Normalizer;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\PropertyInfo\PropertyTypeExtractorInterface;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactoryInterface;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class EntityNormalizer extends ObjectNormalizer
+{
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ?ClassMetadataFactoryInterface $classMetadataFactory = null,
+        ?NameConverterInterface $nameConverter = null,
+        ?PropertyAccessorInterface $propertyAccessor = null,
+        ?PropertyTypeExtractorInterface $propertyTypeExtractor = null
+    ) {
+        $this->entityManager = $entityManager;
+
+        parent::__construct($classMetadataFactory, $nameConverter, $propertyAccessor, $propertyTypeExtractor);
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return (strpos($type, 'App\\Entity\\') === 0) && (is_numeric($data) || is_string($data));
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        return $this->entityManager->find($class, $data);
+    }
+}
