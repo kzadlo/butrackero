496266de968c94eda34e2222ee71d8b2a74eb0ad
Krystian Żądło <krystianzadlo@fejm.pl>
2018-12-20 21:08:29 +0100

    API Validator was implemented

---
+++ src/Balance/Validator/AbstractBalanceValidator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Balance\Validator;
+
+use App\Balance\Model\BalanceEntityInterface;
+
+abstract class AbstractBalanceValidator implements ValidationInterface
+{
+    private $errors = [];
+
+    public function isValid(): bool
+    {
+        return $this->isArrayEmpty($this->getErrors());
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    protected function addError(string $name, string $message): void
+    {
+        $this->errors[$name] = $message;
+    }
+
+    public function isObjectExists(?BalanceEntityInterface $object): bool
+    {
+        return !$this->isNull($object);
+    }
+
+    public function isArrayEmpty(array $array): bool
+    {
+        return empty($array);
+    }
+
+    public function hasArrayKey(string $key, array $array): bool
+    {
+        return array_key_exists($key, $array);
+    }
+
+    public function isGreaterThanZero(float $number): bool
+    {
+        return $number > 0.00;
+    }
+
+    public function isNull($value): bool
+    {
+        return is_null($value);
+    }
+
+    public function isFloat($value): bool
+    {
+        return is_float($value);
+    }
+
+    public function isInt($value): bool
+    {
+        return is_int($value);
+    }
+}
---
+++ src/Balance/Validator/ValidationInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Balance\Validator;
+
+interface ValidationInterface
+{
+    CONST MESSAGE_OBJECT_NOT_EXISTS = "Object with this id doesn't exist";
+    CONST MESSAGE_ARRAY_IS_EMPTY = "This array can't be empty";
+    CONST MESSAGE_KEY_NOT_EXISTS = "This key must declared";
+    CONST MESSAGE_IS_LESS_OR_EQUAL_ZERO = "This value has to be greater than zero";
+    CONST MESSAGE_IS_NULL = "This value can't be null";
+    CONST MESSAGE_IS_NOT_FLOAT = "This value has to be float";
+    CONST MESSAGE_IS_NOT_INT = "This value has to be int";
+
+    public function validate(array $object): void;
+}
